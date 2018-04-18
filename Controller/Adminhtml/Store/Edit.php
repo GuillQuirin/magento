3f5ccff7b956ae0cc1a1de6789bec550cadbdec7
Reconnois <GUI.QUIRIN@hotmail.fr>
2018-04-18 11:57:39 +0200

    Correction et je sais pas pk

--- Controller/Adminhtml/Store/Edit.php
+++ Controller/Adminhtml/Store/Edit.php
@@ -2,39 +2,17 @@
 
 namespace Esgi\Storelocator\Controller\Adminhtml\Store;
 
-use Esgi\Storelocator\Controller\Adminhtml\Store;
+use Magento\Backend\App\Action;	
+use Esgi\Storelocator\Model\Store as Store;
 
-class Edit extends Store
+class Edit extends \Magento\Backend\App\Action
 {
    /**
      * @return void
      */
    public function execute()
    {
-        $newsId = $this->getRequest()->getParam('id');
-        $model = $this->_newsFactory->create();
-
-        if ($newsId) {
-            $model->load($newsId);
-            if (!$model->getId()) {
-                $this->messageManager->addError(__('This news no longer exists.'));
-                $this->_redirect('*/*/');
-                return;
-            }
-        }
-
-        // Restore previously entered form data from session
-        $data = $this->_session->getNewsData(true);
-        if (!empty($data)) {
-            $model->setData($data);
-        }
-        $this->_coreRegistry->register('storelocator_store', $model);
-
-        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
-        $resultPage = $this->_resultPageFactory->create();
-        $resultPage->setActiveMenu('Esgi_Storelocator::main_menu');
-        $resultPage->getConfig()->getTitle()->prepend(__('Simple News'));
-
-        return $resultPage;
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();	
    }
 }
